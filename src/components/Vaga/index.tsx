ace5b1812790dc8f834fb23d7e744dbb9c3f20d6
EstevaoCa <tevo.castro14@gmail.com>
2024-11-06 15:28:32 -0300

    Trocando para Style componentes

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,5 @@
-import styles from './Vaga.module.css'
+import { Container } from '../Hero/style'
+import { Vagaconst, VagaLink, VagaTitulo } from './style'
 
 type Props = {
   titulo: string
@@ -11,21 +12,21 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
-    <ul>
-      <li>Localizacao: {props.localizacao}</li>
-      <li>Senioridade: {props.nivel}</li>
-      <li>Tipo de contratacao: {props.modalidade}</li>
-      <li>
-        Salário: {props.salarioMin} - {props.salarioMax}
-      </li>
-      <li>Requisitos: {props.requisitos.join(', ')}</li>
-    </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+  <Container>
+    <Vagaconst>
+      <VagaTitulo>{props.titulo}</VagaTitulo>
+      <ul>
+        <li>Localizacao: {props.localizacao}</li>
+        <li>Senioridade: {props.nivel}</li>
+        <li>Tipo de contratacao: {props.modalidade}</li>
+        <li>
+          Salário: {props.salarioMin} - {props.salarioMax}
+        </li>
+        <li>Requisitos: {props.requisitos.join(', ')}</li>
+      </ul>
+      <VagaLink href="#">Ver detalhes e candidatar-se</VagaLink>
+    </Vagaconst>
+  </Container>
 )
 
 export default Vaga
